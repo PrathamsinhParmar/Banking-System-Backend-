147d1791ef90f32f36bf24e0afc4840a59f5b4cd
Pratham <prathamsinhparmar0@gmail.com>
2026-05-20 18:47:19 +0530

    Dummy get/ API created

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
+app.get('/', (req, res)=>{
+    res.send("Banking System Is Up & Running !")
+})
+
 app.use('/api/auth', authRouter)
 
 app.use('/api/accounts', accountRouter)
